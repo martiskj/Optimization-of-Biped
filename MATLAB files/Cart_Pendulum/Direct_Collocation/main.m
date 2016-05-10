9a97bb16617bdfffcca7bd0c34f9e493e99552d9
Martin Hals <martinhals@169-231-98-178.wireless.ucsb.edu>
2016-05-10 15:46:41 -0700

    Started writing direct collocation for cart-pandulum system

--- MATLAB files/Cart_Pendulum/Direct_Collocation/main.m
+++ MATLAB files/Cart_Pendulum/Direct_Collocation/main.m
@@ -3,14 +3,14 @@ clear all;
 close all;
 
 %% Set parameters and initial guess
-[m, k, g] = set_parameters();
+[M, mp, Lc, g, mu, Jp] = set_parameters();
 
 % Need to include the timespan in the optimization problem
 T_max = 15;
 N = 50;
 h_k = T_max/(N-1);
 t = (0:h_k:T_max);
-parameters = [m, k, g, h_k];
+parameters = [M, mp, Lc, g, Jp];
 
 x_guess = cos(t);
 x_dot_guess = [0, diff(x_guess)./h_k];
--- MATLAB files/Cart_Pendulum/Direct_Collocation/set_parameters.m
+++ MATLAB files/Cart_Pendulum/Direct_Collocation/set_parameters.m
@@ -1,5 +1,8 @@
-function [m,k,g] = set_parameters()
+function [M, mp, Lc, g, mu, Jp] = set_parameters()
 
-m = 1;
-k = 1;
-g = 9.81;
+M = 5; % Cart mass
+mp = 1; % Pendulum mass
+Lc = 1; % Length to center of mass of pendulum
+g = 9.81; % Gravity
+mu = 0.1 % Coefficient of friction
+Jp = 0; % to be set... Jp=0 means pendulum has a "point mass"
