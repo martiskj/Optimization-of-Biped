31ad978ac9f8ca26a8d47691f9b1281b1d432cfe
martiskj <martiskj@stud.ntnu.no>
2016-05-16 21:52:14 -0700

    made new folder

--- MATLAB files/Hopper/energy conserved/main.m
+++ MATLAB files/Hopper/energy conserved/main.m
@@ -1,13 +1,12 @@
-M = 5;
 m = 1;
 k = 60;
 xs = 2;
 g = 9.81;
 tMax = 5; 
 
-parameters = [k, xs, g, M, m];
+parameters = [m, k, xs, g,];
 tspan = [0 tMax];
-x_init = [10;0;8;0];
+x_init = [4;0];
 [tsim, solution] = ode45(@(t,x) dynamics(x,t,parameters), tspan, x_init);
 
 %%
