95d780a9a19af823f6dfae0d22ec77f4cbc940ae
martiskj <martiskj@stud.ntnu.no>
2016-05-19 13:28:00 -0700

    Added Accuracy vs. no of points in optimization comparison

--- MATLAB files/Hopper/energy conserved/optimization_comparisons.m
+++ MATLAB files/Hopper/energy conserved/optimization_comparisons.m
@@ -12,7 +12,7 @@ tic;
 main_shooting(x_init);
 runTimeS = toc;
 
-samplePoints = [5, 10];%, 100, 200];
+samplePoints = [25, 50, 75, 100, 125];
 runTimeDC = zeros(1, length(samplePoints));
 figure;
 for i = 1:length(samplePoints)
@@ -22,13 +22,15 @@ for i = 1:length(samplePoints)
     runTimeDC(i) = toc;
     subplot(length(samplePoints),1, i);
     plot(linspace(0, tMax, 200), optimalSolutions.(strcat('N',int2str(samplePoints(i)))))
+    grid on;
     xlabel('Time (s)');
     ylabel('Position (m)');
     title(sprintf('Direct Collocation, N=%d', samplePoints(i)));
+    axis([0, inf, 0, inf]);
 end
 
 %% Timecomparison
-axis([0, inf, 0, inf]);
+
 figure;
 plot(samplePoints, runTimeDC, '-bo', samplePoints, ones(1,length(samplePoints))*runTimeS, '-ro');
 xlabel('Sample Points');
@@ -36,7 +38,6 @@ ylabel('Time (s)');
 legend('Direct collocation','Shooting');
 
 %% Accuracy comparison
-
 time = linspace(0,parameters(5),200);
 accuracies = zeros(1, length(samplePoints));
 figure;
@@ -45,7 +46,18 @@ for i = 1:length(samplePoints)
    accuracies.(strcat('N',int2str(samplePoints(i)))) = calc_accuracy(current_solution, parameters);
    subplot(1,length(samplePoints),i)
    plot(time, accuracies.(strcat('N',int2str(samplePoints(i))))(1,:))
-   xlabel('time (s)');
-   ylabel('error')
+   xlabel('Time (s)');
+   ylabel('Error')
    title(sprintf('Accuracy, N = %d', samplePoints(i)))
 end
+
+figure;
+hold on;
+for i = 1:length(samplePoints)
+   plot(samplePoints(i), sum(accuracies.(strcat('N',int2str(samplePoints(i))))(1,:)),'bo-','MarkerFaceColor','b');
+end
+grid on;
+xlabel('Points (N)')
+ylabel('Error')
+title('Accuracy vs. Points')
+    
