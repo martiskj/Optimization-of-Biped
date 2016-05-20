b435be9958234efa87ba20a4bf3139bc2c2d5f38
Martin Hals <martinhals@Martins-MacBook-Pro.local>
2016-05-19 18:59:18 -0700

    Implemented comparison for shooting and direct collocation for energy not conserved

---
+++ MATLAB files/Hopper/energy not conserved/direct_collocation/optimization.m
@@ -0,0 +1,22 @@
+function solution = optimization(x_init, parameters)
+
+%% Optimization by direct collocation
+% Uses trapezoid method (assumes dynamics are linear between grid points)
+% Possible to try a more sophisticated approach
+
+
+%% Try to find the highest possible altitude for a lossless mass-spring jumper
+% Path constraints (no constraints on velocity)
+x_lb = zeros(2,length(x_init));
+x_lb(1,:) = 0;
+x_lb(2,:) = -inf;
+% x_lb(end,end) = 0;
+x_ub = zeros(2,length(x_init));
+x_ub(1,:) = 6;
+x_ub(2,:) = inf;
+% x_ub(end,end) = 20;
+
+optionsFMINCON = optimoptions(@fmincon, 'Algorithm', 'interior-point', 'Display', 'iter','maxFunEvals', 1e4);
+optimal_trajectory = fmincon(@objective_function, x_init, [],[],[],[], x_lb, x_ub, @(x) collocation_constraints(x,parameters), optionsFMINCON);
+
+solution = optimal_trajectory;
---
+++ MATLAB files/Hopper/energy not conserved/get_parameters.m
@@ -0,0 +1,9 @@
+function parameters = get_parameters()
+    m = 1;
+    k = 60;
+    xs = 2;
+    g = 9.81;
+    b = 1;
+    tMax = 5; 
+    parameters = [m, k, xs, g, b, tMax];
+end
