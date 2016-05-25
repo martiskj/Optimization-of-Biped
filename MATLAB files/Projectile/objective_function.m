c09e699d883186077173f985d6e29f562c8d53b1
Martin Hals <marthals@stud.ntnu.no>
2016-05-25 13:21:48 -0700

    Change objective function to energy used to launch the projectile

--- MATLAB files/Projectile/objective_function.m
+++ MATLAB files/Projectile/objective_function.m
@@ -1,15 +1,9 @@
 function cost = objective_function(x)
 
 %% Try to hit the target
-% The cost is the shortest distance the projectile ever is from the goal 
+% The cost is the least amount of energy neede to launch the vehicle
 
-target_x = 10;
-target_y = 0;
-
-cost = sqrt((target_x - x(1,1))^2 + (target_y - x(3,1))^2);
-for i = 2:length(x)-1
-    new_cost = sqrt((target_x - x(1,i))^2 + (target_y - x(3,i))^2);
-    if(new_cost < cost)
-        cost = new_cost;
-    end
-end
+dx_0 = x(2,1);
+dy_0 = x(4,1);
+energy = sqrt(dx_0^2 + dy_0^2);
+cost = energy;
