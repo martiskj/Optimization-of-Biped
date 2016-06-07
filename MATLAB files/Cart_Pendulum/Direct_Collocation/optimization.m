9ec74e9f08b67c5ca77354671875fce583b6fffc
Martin Hals <marthals@stud.ntnu.no>
2016-06-07 16:16:23 -0700

    Made the cart velocity, dx, unconstrained

--- MATLAB files/Cart_Pendulum/Direct_Collocation/optimization.m
+++ MATLAB files/Cart_Pendulum/Direct_Collocation/optimization.m
@@ -10,7 +10,7 @@ function solution = optimization(x_init, parameters)
     x_lb(1,:) = -pi/2;  %theta
     x_lb(2,:) = -2*pi;  %dtheta
     x_lb(3,:) = -inf;   %x
-    x_lb(4,:) = -5;     %dx
+    x_lb(4,:) = -inf;   %dx
     x_lb(5,:) = -0;     %y
     x_lb(6,:) = -0;     %dy
     x_lb(7,:) = -inf;   %taup
@@ -20,13 +20,13 @@ function solution = optimization(x_init, parameters)
     x_ub(1,:) = pi/2;   %theta
     x_ub(2,:) = 2*pi;   %dtheta
     x_ub(3,:) = inf;    %x
-    x_ub(4,:) = 5;      %dx
+    x_ub(4,:) = inf;    %dx
     x_ub(5,:) = 0;      %y
     x_ub(6,:) = 0;      %dy
     x_ub(7,:) = inf;    %taup
     x_ub(end, end) = 10;%time
 
-    optionsFMINCON = optimoptions(@fmincon, 'Algorithm', 'interior-point', 'Display', 'iter', 'maxFunEvals', 6e4);
+    optionsFMINCON = optimoptions(@fmincon, 'Algorithm', 'interior-point', 'Display', 'iter', 'maxFunEvals', 1e6);
     optimal_trajectory = fmincon(@objective_function, x_init, [],[],[],[], x_lb, x_ub, @(x) collocation_constraints(x,parameters), optionsFMINCON);
 
     solution = optimal_trajectory;
